c4c33913f2d9cf11bda0d81ac998a0e18eced7eb
nayan-404 <rahathossain33g@gmail.com>
2024-12-05 09:44:38 +0600

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   ownerNumber: "+8801615298449",//Your Number
   admin: ["5920596290"], // your id
   prefix: "/", // dont change prefix 
-  telegramBotToken: "Your_Bot_Token",//Your Bot Tokens
+  telegramBotToken: "7503293598:AAFpNOB0O3aCvD1Ghyk4VIqn7a3P8cGWuoQ",//Your Bot Tokens
   imageUrl: "https://i.postimg.cc/WpmJhNVg/received-383729254132460.jpg",//Thumbnail Url
   port: process.env.PORT || 8053 || 8053, // port
   mess: {
